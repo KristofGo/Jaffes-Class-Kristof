6ea450d8ff69a72d7a82be79bb927a2c3ea47ed3
KristofGo <143124415+KristofGo@users.noreply.github.com>
2023-09-26 11:32:15 -0700

    Add files via upload

---
+++ IfStatements.java
@@ -0,0 +1,24 @@
+public class IfStatements {
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+    
+    if (a == 5) { 
+        System.out.println("a is equal to 5");
+        System.out.println("a is still equal to 5");
+    } else if (b == 2) {           
+        System.out.println("b is equal to 2");
+        System.out.println("b is still equal to 2");
+    } else {
+        if (c == 1);
+        System.out.println("c is equal to 1");
+        System.out.println("c is still equal to 1");
+        
+    if (d == -8) 
+        System.out.println("d is equal to -8");
+        System.out.println("d is still equal to -8");
+    }
+  }
+}
